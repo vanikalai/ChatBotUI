09c6d8f8edeb8032d685fe8be4a804603d3b5c0a
manikandan-atos <manikandan.atossyntel@gmail.com>
2021-02-05 16:25:05 +0530

    Chatbot UI and API changes

--- src/App.js
+++ src/App.js
@@ -48,58 +48,81 @@ class App extends Component{
 	constructor() {
 		super();
 		this.state = {
-			output: "",
+			output: "Howdy",
 			questions: ["Hi! I'm a bot. What's up???"],
-			answers: [] 
+			answers: [],
+			conversation: [{
+				question: "Hi! I'm a bot. What's up???",
+				answer: ''
+			}] 
 		};
 	  }
 
 	  componentDidMount() {
-		this.getRes = this.getRes.bind(this);
+		this.getRes();		
+		
 	  }
 
 	  handleChange=(e) => {
 		this.setState({ output: e.target.value });
 	  }
 
-	  getRes=(target)=>{
+	  getOutPut = () => {
+		let output = this.state.conversation.map((item, index) => {
+			return(
+			<div>
+			<div className="user-message" key={index}>
+				<div className="message">{item.question}</div>					
+			</div>
+			 <div className="message">{item.answer}</div>
+		</div>
+		)	
+		});
+		return output;
+	  }
+
+	  getRes=()=>{
 		console.log(this.state.output);
+		const username = 'sbaiserviceuser'
+		const password = 'syntbot123$'
+
+		const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
 		let headers = {
+			'Authorization': `Basic ${token}`,
 			'Access-Control-Allow-Origin':'*',
-			'Content-Type': 'application/json'
+			'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+			'Access-Control-Allow-Headers': 'Content-Type',
+			'Content-Type': 'application/json',
+			
 		}
-		let url = "http://13.75.106.59/syntbots-ai/v3/converse_api/prediction"; 
+		const url = "http://13.75.106.59/syntbots-ai/v3/converse_api/prediction"; 
 		const body = JSON.stringify({
 			"sessionid": "017",
 			"conversation_name": "hire_1",
-			"user_query": "Howdy"   
+			"user_query": this.state.output   
 		});
 		axios.post(url, body, {headers}).then(response => {
 			console.log(response.data);
+			let conversation = {
+				question: this.state.output,
+				answer: response.data.text
+			}
 			this.setState({
 				questions: [...this.state.questions, this.state.output],
-				answers:[...this.state.answers, response.data.text]
+				answers:[...this.state.answers, response.data.text],
+				conversation: [...this.state.conversation, conversation]
 			 })
+			 
 		})		
 		}
 
   render(){
-	  console.log(this.state.questions);
-	  console.log(this.state.answers);
+	  console.log('Conversation', this.state.conversation);
 	return (
 		<>
 
 		<div className="chat-output" id="chat-output">
-			{this.state.questions.map((item, index) => (
-				<div className="user-message" key={index}>
-				<div className="message">{item}</div>
-			</div>
-			))}
-		
-			{this.state.answers.map((item, index) => (
-				<div className="message" key={index}>{item}</div>
-
-			))}
+			{this.getOutPut()}
 			<div className="chat-input">
 			  
 			  <input type="text" id="user-input" className="user-input" onChange={ this.handleChange } placeholder="Talk to the bot." />
@@ -107,22 +130,6 @@ class App extends Component{
   
 		  </div>
 		</div>
-
-		
-		{/* <div className="chat-output" id="chat-output">
-				  <div className="user-message">
-					<div className="message">Hi! I'm a bot. What's up?</div>
-				  </div>
-					  {this.state.output}
-				</div>
-	
-			<div className="chat-input">
-			  
-				<input type="text" id="user-input" className="user-input" placeholder="Talk to the bot." />
-				<button onClick={this.getRes}>ENTER</button>
-	
-			</div> */}
-	
 		</>
 	  )
   }
